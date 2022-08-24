c958b240d60055968b29a7254b05f2a011548ac6
eoghanriley <eoghan.riley@gmail.com>
2022-08-24 15:28:59 -0400

    switched to jit mode for tailwind.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: 'jit',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {}
 	},
-	plugins: [],
-}
+	plugins: []
+};
