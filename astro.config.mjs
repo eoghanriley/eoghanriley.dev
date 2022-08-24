7a9e8ef80492561311819cffebe8861bdd1ba267
eoghanriley <eoghan.riley@gmail.com>
2022-08-24 17:13:28 -0400

    added astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+	integrations: [tailwind(), svelte()],
+	vite: {
+		ssr: {
+			external: ['svgo']
+		}
+	}
 });
+
